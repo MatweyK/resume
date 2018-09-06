350d815fd17555f45877c8b34937d59c74a714d6
MatweyK <matweykrysak@mail.ru>
2018-09-06 13:36:06 +0300

    form fixed

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
 
 $recepient = "matweykrysak@gmail.com";
-$sitename = "https://andrew746746.000webhostapp.com/";
+$sitename = "https://resume7.000webhostapp.com/";
 
 $name = trim($_POST["name"]);
-$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
 $text = trim($_POST["text"]);
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$message = "Имя: $name \nТелефон: $email \nТекст: $text";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
